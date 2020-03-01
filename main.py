29a56ae4ada48bdf42bc5a7fd871f06371caa808
Jack <hui2@pdx.edu>
2020-03-01 12:49:21 -0800

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+response = requests.get("https://wowtokenprices.com/current_prices.json")
+jprint(response.json())
